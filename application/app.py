dd6221d208d9c55000af701cf2013a538cfb48bd
Spencer Buja <spencerb@ziprecruiter.com>
2021-03-13 23:36:02 -0800

    app runs on port 8080

--- application/app.py
+++ application/app.py
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
--- application/setup.sh
+++ application/setup.sh
@@ -1,1 +1,3 @@
 sudo ufw allow 8080
+
+sudo /home/cbuja/cs411/application/venv/bin/python3 app.py

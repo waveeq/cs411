4c6bf8fbde6c063c50e525168f8fed7df9b6ba08
JackySa1994 <79820105+JackySa1994@users.noreply.github.com>
2021-04-08 23:15:18 -0400

    Update app.py

--- application/app.py
+++ application/app.py
@@ -58,4 +58,4 @@ if __name__ == '__main__':
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
-    app.run()
+    app.run(host="0.0.0.0", port=8080)

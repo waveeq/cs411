461e90a44a4a83aa1e11703ca34b24cfbc09a4d0
Spencer Buja <spencerb@ziprecruiter.com>
2021-05-10 18:06:55 -0600

    mongo fixed

--- mongo/readMessages.py
+++ mongo/readMessages.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 def get_messages(user_id):
     client = MongoClient()
     db = client.cs411
-    return [x for x in db.Messages.find() if ((x.get("user_id_to") and x["user_id_to"] == user_id) or (x.get("user_id_from") and x["user_id_from"] == user_id))]
+    return [x for x in db.Messages.find() if (("user_id_to" in x and x["user_id_to"] == user_id) or ("user_id_from" in x and x["user_id_from"] == user_id))]
 
 if __name__ == "__main__":
     print(get_messages(0))

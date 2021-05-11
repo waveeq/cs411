9b2b98ed520c1e1d2c220e5837fe0e8ebacc0053
Atif Malik <amalik@Atifs-MacBook-Pro.local>
2021-05-10 22:48:24 -0400

    Fixing user reg

--- application/user.py
+++ application/user.py
@@ -8,29 +8,43 @@ import hmac
 
 
 class User(flask.views.MethodView):
-    def _put_user(self, profile_image_param , user_name_param , email_param , first_name_param, last_name_param, password_param, birth_date_param, country_param):
-        
-        if email_param == None or user_name_param == None or password_param == None :
-            return
+    def _put_user(self, profile_image , user_name , email , first_name, last_name, password, birth_date, country):
+
 
         mysqlUN = settings.MYSQL_USER
         mysqlPW = settings.MYSQL_PASSWORD
         dbname = settings.MYSQL_DB
         dbhost = settings.MYSQL_HOST
+        
 
-        hashed_password = self.hash_password(password_param)
         
         myDatabase = 'mysql://%(UN)s:%(PW)s@%(host)s/%(name)s?charset=utf8&use_unicode=1' % \
         {'UN': mysqlUN, 'PW': mysqlPW, 'host': dbhost, 'name': dbname}
         engine = create_engine(myDatabase, pool_recycle=3600)  
 
-        with engine.connect() as connection:
-            result = connection.execute(
-                """
-                INSERT INTO USER  (profile_image, user_name, email, first_name, last_name, password, birth_date, country)
-                VALUES ("%s","%s", "%s", "%s", "%s", "%s" , "%s" , "%s" )
-                """,
-                [profile_image_param , user_name_param , email_param , first_name_param, last_name_param, hashed_password, birth_date_param, country_param])
+
+        connection = engine.connect()
+        
+        
+        if email == None or user_name == None or password == None :
+            return
+
+        hashed_password = self.hash_password(password_param)
+        
+
+        with connection:
+
+
+            result = connection.execute('select uid, email, user_name from User where email = %s or user_name = %s', [uid, email, user_name])
+
+            data =  [[str(row[0])] for row in result]
+
+            if data:
+                return flask.abort(404)
+
+            else:
+                connection.execute('INSERT INTO User  (profile_image, user_name, email, first_name, last_name, password, birth_date, country) VALUES ("%s","%s", "%s", "%s", "%s", "%s" , "%s" , "%s")', 
+                [profile_image, user_name, email_param , first_name, last_name, hashed_password, birth_date, country])
             return Response(status=200)
 
     def hash_password(self,password):
@@ -38,16 +52,17 @@ class User(flask.views.MethodView):
         
     def put(self):
         
-        profile_image_param = flask.request.args.get('profile_image')
-        user_name_param  = flask.request.args.get('user_name')
-        email_param  = flask.request.args.get('email')
-        first_name_param = flask.request.args.get('first_name')
-        last_name_param = flask.request.args.get('last_name')
-        password_param = flask.request.args.get('password')
-        birth_date_param = flask.request.args.get('birth_date')
-        country_param = flask.request.args.get('country')                        
+        profile_image = flask.request.args.get('profile_image')
+        user_name  = flask.request.args.get('user_name')
+        email  = flask.request.args.get('email')
+        first_name = flask.request.args.get('first_name')
+        last_name = flask.request.args.get('last_name')
+        password = flask.request.args.get('password')
+        birth_date = flask.request.args.get('birth_date')
+        country = flask.request.args.get('country')                        
 
-        return self._put_User(profile_image_param, user_name_param, email_param, first_name_param, last_name_param, password_param, birth_date_param, country_param)
+        return self._put_User(profile_image, user_name, email, first_name, last_name, password, birth_date, country)
+
 
     def _get_user(self,uid):
         mysqlUN = settings.MYSQL_USER
